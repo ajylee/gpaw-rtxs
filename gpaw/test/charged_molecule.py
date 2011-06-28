f95689bafb991663c922d1cc732e2eeaec6db66e
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2011-06-28 09:31:02 +0000

    Test for high spin Li2-.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8188 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/charged_molecule.py
+++ gpaw/test/charged_molecule.py
@@ -13,7 +13,7 @@ d=2.0   # does not matter
 # fixmagmom is not needed - but it speeds up the calculation
 # about 20%
 calc=GPAW(occupations=FermiDirac(width=0.1, fixmagmom=True),
-        charge=1.0,
+        charge=-1.0,
         h=gs,
         mixer=Mixer(beta=0.75, nmaxold=2, weight=50.0),
         convergence={'energy':0.5, 'eigenstates': 1.e-0,
@@ -21,8 +21,8 @@ calc=GPAW(occupations=FermiDirac(width=0.1, fixmagmom=True),
 atoms = Atoms('Li2', positions=[(0,0,0),(0,0,d)], pbc=False)
 atoms.center(vacuum=vac)
 mm = [1] * 2
-mm[0] = 1.
-mm[1] = 0.
+mm[0] = 2.
+mm[1] = 1.
 atoms.set_initial_magnetic_moments(mm)
 atoms.set_calculator(calc)
 atoms.get_potential_energy()
