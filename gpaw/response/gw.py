d4bf0046043d967dbb22f6e7defc33ca62d91885
falh <falh@6419b465-d902-0410-997c-be2266baf723>
2011-07-25 15:11:07 +0000

    Merged both methods.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8264 6419b465-d902-0410-997c-be2266baf723

--- gpaw/response/gw.py
+++ gpaw/response/gw.py
@@ -22,6 +22,7 @@ class GW(BASECHI):
                  w=None,
                  ecut=150.,
                  eta=0.1,
+                 full_frequency=False,
                  txt=None,
                 ):
 
@@ -30,6 +31,7 @@ class GW(BASECHI):
         self.vcut = None
         self.bands = bands
         self.kpoints = kpoints
+        self.full_frequency = full_frequency
 
     def initialize(self):
 
@@ -85,6 +87,7 @@ class GW(BASECHI):
             self.gwbands_n = self.bands
 
         self.alpha = 1j/(2*pi) * self.dw / (self.vol * self.nkpt)
+
         # print init
         self.print_gw_init()
         
@@ -96,7 +99,7 @@ class GW(BASECHI):
         self.printtxt("calculating Sigma")
 
         Sigma_kn = np.zeros((self.gwnkpt, self.gwnband), dtype=float)
-        Sigmader_kn = np.zeros((self.gwnkpt, self.gwnband), dtype=float)
+        dSigma_kn = np.zeros((self.gwnkpt, self.gwnband), dtype=float)
         Z_kn = np.zeros((self.gwnkpt, self.gwnband), dtype=float)
 
         t0 = time()
@@ -111,12 +114,12 @@ class GW(BASECHI):
             t_w += t2 - t1
 
             # get self energy
-            S, Sder = self.get_self_energy(df, W_wGG)
+            S, dS = self.get_self_energy(df, W_wGG)
             t3 = time() - t2
             t_selfenergy += t3
             
             Sigma_kn += S
-            Sigmader_kn += Sder
+            dSigma_kn += dS
             
             del df
             self.timing(iq, t0, self.nq_local, 'iq')
@@ -126,9 +129,9 @@ class GW(BASECHI):
 
         self.qcomm.barrier()
         self.qcomm.sum(Sigma_kn)
-        self.qcomm.sum(Sigmader_kn)
+        self.qcomm.sum(dSigma_kn)
 
-        Z_kn = 1. / (1. + Sigmader_kn)
+        Z_kn = 1. / (1. - dSigma_kn)
 
         # exact exchange
         t0 = time()
@@ -138,20 +141,52 @@ class GW(BASECHI):
 #        for k in range(self.nkpt):
 #            print self.kd.bz2ibz_k[k], Sigma_kn[k,:] * Hartree
 
-        Qp_kn = e_kn + Z_kn * (Sigma_kn + e_xx - v_kn)
+        QP_kn = e_kn + Z_kn * (Sigma_kn + e_xx - v_kn)
 
         # finish
-        self.print_gw_finish(e_kn, v_kn, e_xx, Sigma_kn, Z_kn, Qp_kn)
+        self.print_gw_finish(e_kn, v_kn, e_xx, Sigma_kn, Z_kn, QP_kn)
 
 
     def get_self_energy(self, df, W_wGG):
 
         Sigma_kn = np.zeros((self.gwnkpt, self.gwnband), dtype=float)
-        Sigmader_kn = np.zeros((self.gwnkpt, self.gwnband), dtype=float)
+        dSigma_kn = np.zeros((self.gwnkpt, self.gwnband), dtype=float)
         E_f = self.calc.get_fermi_level() / Hartree
 
-        Wbackup_wG0 = W_wGG[:,:,0].copy()
-        Wbackup_w0G = W_wGG[:,0,:].copy()
+        # prepare optical limit for both methods
+        if df.optical_limit:
+            tmp_wG = np.zeros((self.Nw, self.npw), dtype=complex)
+            q = np.array([0.0001,0,0])
+            for jG in range(1, self.npw):
+                qG = np.dot(q+self.Gvec_Gc[jG], self.bcell_cv)
+                tmp_wG[:,jG] = self.dfinvG0_wG[:,jG] / np.sqrt(np.inner(qG,qG))
+                const = 1./pi*self.vol*(6*pi**2/self.vol)**(2./3.)
+            tmp_wG *= const
+            tmp_w = 2./pi*(6*pi**2/self.vol)**(1./3.) * self.dfinvG0_wG[:,0] * self.vol
+        print np.shape(W_wGG)
+
+        if self.full_frequency: #method 1
+            Wbackup_wG0 = W_wGG[:,:,0].copy()
+            Wbackup_w0G = W_wGG[:,0,:].copy()
+
+        else: #method 2, perform Hilbert transform
+#            Cplus_wGG = np.zeros((self.Nw, self.npw, self.npw), dtype=complex)
+#            Cminus_wGG = np.zeros((self.Nw, self.npw, self.npw), dtype=complex)
+            Cplus_wGG = np.zeros_like(W_wGG)
+            Cminus_wGG = np.zeros_like(W_wGG)
+
+            w2_w = np.arange(self.Nw) * self.dw
+            for iw in range(self.Nw):
+                w1 = iw * self.dw
+                w1_w = 1. / (w1 + w2_w + 1j*self.eta) + 1. / (w1 - w2_w + 1j*self.eta)
+                Cplus_wGG[iw] = gemmdot(w1_w, W_wGG, beta=0.0)
+                Cminus_wGG[iw] = gemmdot(w1_w.conj(), W_wGG, beta=0.0)
+            if df.optical_limit:
+                w1_w = 1. / (w2_w + 1j*self.eta) + 1. / (-w2_w + 1j*self.eta)
+                Cplus_0G0 = gemmdot(w1_w, tmp_wG, beta=0.0)
+                Cminus_0G0 = gemmdot(w1_w.conj(), tmp_wG, beta=0.0)
+                Cplus_0G0[0] = gemmdot(w1_w, tmp_w, beta=0.0)
+                Cminus_0G0[0] = gemmdot(w1_w.conj(), tmp_w, beta=0.0)
 
         for i, k in enumerate(self.gwkpt_k): # k is bzk index
             if df.optical_limit:
@@ -160,59 +195,92 @@ class GW(BASECHI):
                 kq_c = df.kd.bzk_kc[k] - df.q_c  # k - q
             
             kq = df.kd.where_is_q(kq_c, df.kd.bzk_kc)            
+            assert df.kq_k[kq] == k
             ibzkpt1 = df.kd.bz2ibz_k[k]
             ibzkpt2 = df.kd.bz2ibz_k[kq]
 
             for j, n in enumerate(self.bands):
                 for m in range(self.nbands):
 
-                    W_wGG[:,:,0] = Wbackup_wG0
-                    W_wGG[:,0,:] = Wbackup_w0G
-
-                    if df.optical_limit:
-                        if n == m:
-                            tmp_wG = np.zeros((self.Nw,self.npw))
-                            q = np.array([0.0001,0,0])
-                            for jG in range(1, self.npw):
-                                qG = np.dot(q+self.Gvec_Gc[jG], self.bcell_cv)
-                                tmp_wG[:,jG] = self.dfinvG0_wG[:,jG] / np.sqrt(np.inner(qG,qG))
-
-                            const = 1./pi*self.vol*(6*pi**2/self.vol)**(2./3.)
-                            tmp_wG *= const
-                            W_wGG[:,:,0] = tmp_wG
-                            W_wGG[:,0,:] = tmp_wG.conj()
-                            W_wGG[:,0,0] = 2./pi*(6*pi**2/self.vol)**(1./3.) \
-                                           * self.dfinvG0_wG[:,0] *self.vol
-                        else:
-                            # to be checked.
-                            W_wGG[:,0,0:] = 0.
-                            W_wGG[:,0:,0] = 0.
-
-                    assert df.kq_k[kq] == k
-                    rho_G = df.density_matrix(m, n, kq)
-
-                    # perform W_wGG * np.outer(rho_G.conj(), rho_G).sum(GG)
-                    W_wG = gemmdot(W_wGG, rho_G, beta=0.0)
-                    C_w = gemmdot(W_wG, rho_G, alpha=self.alpha, beta=0.0,trans='c')
-
-                    # w1 = w - epsilon_m,k-q + i*eta * sgn(epsilon_m,k-q, E_f)
                     if self.f_kn[ibzkpt2,m] < self.ftol: #self.e_kn[ibzkpt2, m] > E_f :
                         sign = 1.
                     else:
                         sign = -1.
-                    w1 = self.e_kn[ibzkpt1, n] - self.e_kn[ibzkpt2,m] + 1j*self.eta*sign
-                    w2_w = np.arange(self.Nw) * self.dw
 
-                    # calculate self energy
-                    tmp_w = 1./(w1- w2_w) + 1./(w1 + w2_w)
-                    Sigma_kn[i,j] += np.real(gemmdot(C_w, tmp_w, beta=0.0))
+                    rho_G = df.density_matrix(m, n, kq)
 
-                    # calculate derivate of self energy with respect to w
-                    tmp_w = 1./(w1- w2_w)**2 + 1./(w1 + w2_w)**2
-                    Sigmader_kn[i,j] += np.real(gemmdot(C_w, tmp_w, beta=0.0))
+                    if self.full_frequency: #method 1
+                        W_wGG[:,:,0] = Wbackup_wG0
+                        W_wGG[:,0,:] = Wbackup_w0G
+
+                        if df.optical_limit:
+                            if n==m:
+                                W_wGG[:,:,0] = tmp_wG
+                                W_wGG[:,0,:] = tmp_wG.conj()
+                                W_wGG[:,0,0] = tmp_w
+                            else:
+                                # to be checked.
+                                W_wGG[:,0,0:] = 0.
+                                W_wGG[:,0:,0] = 0.
+
+                        # perform W_wGG * np.outer(rho_G.conj(), rho_G).sum(GG)
+                        W_wG = gemmdot(W_wGG, rho_G, beta=0.0)
+                        C_w = gemmdot(W_wG, rho_G, alpha=self.alpha, beta=0.0,trans='c')
+
+                        # w1 = w - epsilon_m,k-q + i*eta * sgn(epsilon_m,k-q, E_f)
+                        w1 = self.e_kn[ibzkpt1, n] - self.e_kn[ibzkpt2,m] + 1j*self.eta*sign
+                        w2_w = np.arange(self.Nw) * self.dw
+
+                        # calculate self energy
+                        w1_w = 1./(w1 - w2_w) + 1./(w1 + w2_w)
+                        Sigma_kn[i,j] += np.real(gemmdot(C_w, w1_w, beta=0.0))
+
+                        # calculate derivate of self energy with respect to w
+                        w1_w = 1./(w1   - w2_w)**2 + 1./(w1 + w2_w)**2
+                        dSigma_kn[i,j] -= np.real(gemmdot(C_w, w1_w, beta=0.0))
                     
+                    else: #method 2
+                        if not self.e_kn[ibzkpt2,m] - self.e_kn[ibzkpt1,n] == 0:
+                            sign *= np.sign(self.e_kn[ibzkpt1,n] - self.e_kn[ibzkpt2,m])
 
-        return Sigma_kn, Sigmader_kn 
+                        # find points on frequency grid
+                        w0 = self.e_kn[ibzkpt2,m] - self.e_kn[ibzkpt1,n]
+                        w0_id = np.abs(int(w0 / self.dw))
+                        w1 = w0_id * self.dw
+                        w2 = (w0_id + 1) * self.dw
+
+                        # treat optical limit
+                        if sign == 1:
+                            C_wGG = Cplus_wGG.copy()
+                            if df.optical_limit:
+                                if n==m:
+                                    C_wGG[:,:,0] = Cplus_0G0
+                                    C_wGG[:,0,:] = Cplus_0G0.conj()
+                                else:
+                                    C_wGG[:,0,0:] = 0.
+                                    C_wGG[:,0:,0] = 0.
+                        if sign == -1:
+                            C_wGG = Cminus_wGG.copy()
+                            if df.optical_limit:
+                                if n==m:
+                                    C_wGG[:,:,0] = Cminus_0G0
+                                    C_wGG[:,0,:] = Cminus_0G0.conj()
+                                else:
+                                    C_wGG[:,0,0:] = 0.
+                                    C_wGG[:,0:,0] = 0.
+
+                        # perform C_wGG * np.outer(rho_G.conj(), rho_G).sum(GG)
+                        Sw1_G = gemmdot(C_wGG[w0_id], rho_G, beta=0.0)
+                        Sw1 = gemmdot(Sw1_G, rho_G, alpha=self.alpha, beta=0.0, trans='c')
+                        Sw2_G = gemmdot(C_wGG[w0_id + 1], rho_G, beta=0.0)
+                        Sw2 = gemmdot(Sw2_G, rho_G, alpha=self.alpha, beta=0.0, trans='c')
+
+                        # calculate self energy and derivative via linearization
+                        Sw0 = (w2-np.abs(w0))/self.dw * Sw1 + (np.abs(w0)-w1)/self.dw * Sw2
+                        Sigma_kn[i][j] = Sigma_kn[i][j] + np.sign(self.e_kn[ibzkpt1,n] - self.e_kn[ibzkpt2,m])*Sw0
+                        dSigma_kn[i][j] = dSigma_kn[i][j] + np.real((Sw2 - Sw1)/(w2 - w1))
+
+        return Sigma_kn, dSigma_kn 
 
 
     def get_exx(self):
