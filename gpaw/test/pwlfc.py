13874caf1c5bb2f504a8676a8dde9b18e1392b55
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2011-08-02 15:58:48 +0000

    Fix test.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8292 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/pwlfc.py
+++ gpaw/test/pwlfc.py
@@ -1,50 +1,52 @@
 import numpy as np
 
-from gpaw.wavefunctions.pw import PWDescriptor, PWLFC, RealSpacePWLFC
 from gpaw.test import equal
 from gpaw.grid_descriptor import GridDescriptor
 from gpaw.spline import Spline
 import gpaw.mpi as mpi
 from gpaw.lfc import LocalizedFunctionsCollection as LFC
+from gpaw.wavefunctions.pw import PWDescriptor, PWLFC, RealSpacePWLFC
+
 
 x = 2.0
-rc = 3.0
+rc = 3.5
 r = np.linspace(0, rc, 100)
-s = Spline(1, rc, 2 * x**1.5 / np.pi * np.exp(-x * r**2))
 
 n = 40
 a = 8.0
 gd = GridDescriptor((n, n, n), (a, a, a), comm=mpi.serial_comm)
-c = LFC(gd, [[s]], dtype=complex)
 
-kpts = np.array([(0.25, 0.25, 0.0)])*0
+kpts = np.array([(0.25, 0.25, 0.0)])
 spos_ac = np.array([(0.15, 0.5, 0.95)])
-spos_ac = np.array([(0.5, 0.5, 0.5)])
 
-c.set_k_points(kpts)
-c.set_positions(spos_ac)
-b = gd.zeros(dtype=complex)
-c_ai = {0: np.array([1.9 - 4.5j, 0, 0])}
-c_ai = {0: np.array([1.0 - 0.0j, 0, 0])}
-c.add(b, c_ai, 0)
-print gd.integrate(b)
-b = abs(b)
+for l in range(3):
+    s = Spline(l, rc, 2 * x**1.5 / np.pi * np.exp(-x * r**2))
 
-pd = PWDescriptor(45, gd)
-c = RealSpacePWLFC(LFC(gd, [[s]]), pd)
-c.set_k_points(kpts)
-c.set_positions(spos_ac)
-b2 = pd.zeros(1, dtype=complex)
-c_axi = {0: np.array([[1.0 - 0.0j, 0, 0]])}
-c.add(b2, c_axi, 0)
-print b2[0,0]
-b3 = abs(pd.ifft(b2[0]))
-print abs(b-b3).max()
+    c = LFC(gd, [[s]], dtype=complex)
+    c.set_k_points(kpts)
+    c.set_positions(spos_ac)
+    b = gd.zeros(dtype=complex)
 
-c = PWLFC([[s]], pd)
-c.set_k_points(kpts)
-c.set_positions(spos_ac)
-b2 = pd.zeros(dtype=complex)
-c.add(b2, c_ai, 0)
-b3 = abs(pd.ifft(b2))
-print abs(b-b3).max()
+    c_ai = {0: np.zeros(2 * l + 1, complex)}
+    c_ai[0][0] = 1.9 - 4.5j
+
+    c.add(b, c_ai, 0)
+
+    pd = PWDescriptor(45, gd, kpts)
+    c = RealSpacePWLFC(LFC(gd, [[s]]), pd)
+    c.set_k_points(kpts)
+    c.set_positions(spos_ac)
+    b2 = pd.zeros(1, dtype=complex)
+    c_axi = {0: np.array([c_ai[0]])}
+    c.add(b2, c_axi, 0)
+    b3 = pd.ifft(b2[0]) * c.expikr_qR[0]
+    equal(abs(b-b3).max(), 0, 0.001)
+
+    c3 = PWLFC([[s]], pd)
+    c3.set_k_points(kpts)
+    c3.set_positions(spos_ac)
+    b2 = pd.zeros(dtype=complex)
+    c3.add(b2, c_ai, 0)
+    b4 = pd.ifft(b2) * c.expikr_qR[0]
+    equal(abs(b4-b3).max(), 0, 2e-7)
+
