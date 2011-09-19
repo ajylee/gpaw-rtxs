Fix problem with restart of LCAO calculation.
git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8398 6419b465-d902-0410-997c-be2266baf723

gpaw/io/__init__.py
@@ -700,7 +700,7 @@ def read(paw, reader):
 
     if (nibzkpts != len(wfs.ibzk_kc) or
         nbands != band_comm.size * wfs.mynbands):
-        wfs.eigensolver.error = np.inf
+        paw.scf.reset()
     else:
         # Verify that symmetries for for k-point reduction hasn't changed:
         tol = 1e-12