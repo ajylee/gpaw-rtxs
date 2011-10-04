4fd99221d8a08e558e8cdf5baca5dadf6f076e68
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2011-10-04 15:35:57 +0000

    a potential bug
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8421 6419b465-d902-0410-997c-be2266baf723

--- gpaw/transport/tools.py
+++ gpaw/transport/tools.py
@@ -971,9 +971,14 @@ def save_bias_data_file(Lead1, Lead2, Device):
     density = Device.calc.density
     hamL = Lead1.calc.hamiltonian
     hamR = Lead2.calc.hamiltonian
+    Ef = Device.calc.get_fermi_level()
+    Ef_L = Lead1.calc.get_fermi_level()
+    Ef_R = Lead2.calc.get_fermi_level()
     vt_sG = ham.gd.collect(ham.vt_sG) 
     vt_sG_L = hamL.gd.collect(hamL.vt_sG)
     vt_sG_R = hamR.gd.collect(hamR.vt_sG)
+    vt_sG += Ef_L - Ef
+    vt_sG_R += Ef_L - Ef_R
     vt_sG=np.append(vt_sG_L, vt_sG,axis=3)
     vt_sG=np.append(vt_sG,vt_sG_R,axis=3)
     dH_asp = collect_atomic_matrices(ham.dH_asp, ham.setups,
