210fdad8953981c8b2651ce5bcab31ec4d217c45
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2011-10-11 11:23:38 +0000

    Simplify tests
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8434 6419b465-d902-0410-997c-be2266baf723

--- gpaw/tasks/convergence.py
+++ gpaw/tasks/convergence.py
@@ -3,7 +3,8 @@ import optparse
 import numpy as np
 from ase.atoms import Atoms
 from ase.tasks.task import Task
-from ase.data import covalent_radii, atomic_numbers
+from ase.io import string2index
+from ase.data import covalent_radii
 
 from gpaw import FermiDirac
 
@@ -19,57 +20,56 @@ class ConvergenceTestTask(Task):
         self.taskname = 'convergence'
 
     def build_system(self, name):
-        atoms = Atoms(name * 2,
-                      [(0, 0, 0), (1.0, 1.1, 1.2)],
-                      pbc=True,
-                      cell=(self.L, self.L, self.L))
-        r = covalent_radii[atomic_numbers[name]]
-        atoms.set_distance(0, 1, 2 * r, 0)
-        
-        return atoms
+        symbol = name.split('-')[0]
+        return Atoms(symbol, pbc=True, cell=(self.L, self.L, self.L))
+
+    def run(self, names):
+        names = self.expand(names)
+        newnames = []
+        for g in self.gs:
+            newnames.extend([name + '-' + str(g) for name in names])
+        Task.run(self, newnames)
 
     def calculate(self, name, atoms):
-        data = {}
-        for g in self.gs:
-            atoms.calc.set(gpts=(g, g, g),
-                           occupations=FermiDirac(0.1),
-                           kpts=[1, 1, 1])
-            results = []
-            e = atoms.get_potential_energy()
-            data[g] = [e]
+        g = int(name.split('-')[1])
+        atoms.calc.set(gpts=(g, g, g),
+                       occupations=FermiDirac(0.1),
+                       kpts=[1, 1, 1])
+        e1 = atoms.get_potential_energy()
+        
+        atoms += atoms
+        atoms[1].position = [1.0, 1.1, 1.2]
+        r = covalent_radii[atoms[0].number]
+        atoms.set_distance(0, 1, 2 * r, 0)
 
-        del atoms[1]
-        for g in self.gs:
-            atoms.calc.set(gpts=(g, g, g),
-                           occupations=FermiDirac(0.1),
-                           kpts=[1, 1, 1])
-            results = []
-            e = atoms.get_potential_energy()
-            data[g].append(e)
-
-        return data
+        e2 = atoms.get_potential_energy()
+        
+        return {'e1': e1, 'e2':e2}
 
     def analyse(self):
         self.summary_header = [('name', '')] + [
             ('dE(h=%.2f)' % (self.L / g), 'meV') for g in self.gs]
 
         for name, data in self.data.items():
-            self.results[name] = [1000 * (2 * data[g][1] - data[g][0])
-                                  for g in self.gs]
+            symbol, g = name.split('-')
+            g = int(g)
+            ea = 2 * data['e1'] - data['e2']
+            if symbol not in self.results:
+                self.results[symbol] = [None] * len(self.gs)
+            self.results[symbol][self.gs.index(g)] = ea * 1000
             
     def add_options(self, parser):
         Task.add_options(self, parser)
 
-        egg = optparse.OptionGroup(parser, 'Convergence')
-        egg.add_option('-g', '--grid-point-range', default='20,40',
+        grp = optparse.OptionGroup(parser, self.taskname.title())
+        grp.add_option('-g', '--grid-point-range', default='20:41:4',
                        help='...')
-        parser.add_option_group(egg)
+        parser.add_option_group(grp)
 
     def parse(self, opts, args):
         Task.parse(self, opts, args)
 
-        g1, g2 = (int(x) for x in opts.grid_point_range.split(','))
-        self.gs = range(g1, g2 + 1, 4)
+        self.gs = range(100)[string2index(opts.grid_point_range)]
 
 
 if __name__ == '__main__':
--- gpaw/tasks/eggbox.py
+++ gpaw/tasks/eggbox.py
@@ -1,63 +1,48 @@
 import optparse
 
 import numpy as np
-from ase.tasks.task import Task
 from ase.atoms import Atoms
 
 from gpaw import FermiDirac
+from gpaw.tasks.convergence import ConvergenceTestTask
 
 
-class EggboxTestTask(Task):
-    def __init__(self, g1=20, g2=40, L=4.0, **kwargs):
-        self.gs = range(g1, g2 + 1, 4)
-        
-        self.L = L
-
-        Task.__init__(self, calcwrapper='gpaw', **kwargs)
+class EggboxTestTask(ConvergenceTestTask):
+    def __init__(self, **kwargs):
+        ConvergenceTestTask.__init__(self, **kwargs)
         
         self.taskname = 'eggbox'
 
-    def build_system(self, name):
-        atoms = Atoms(name, pbc=True, cell=(self.L, self.L, self.L))
-        return atoms
-
     def calculate(self, name, atoms):
-        data = {}
-        for g in self.gs:
-            atoms.calc.set(gpts=(g, g, g),
-                           occupations=FermiDirac(0.1),
-                           kpts=[1, 1, 1])
-            results = []
-            for i in range(25):
-                x = self.L / g * i / 48
-                atoms[0].x = x
-                e = atoms.calc.get_potential_energy(atoms,
-                                                    force_consistent=True)
-                f = atoms.get_forces()[0, 0]
-                results.append((e, f))
-            data[g] = results
-        return data
+        g = int(name.split('-')[1])
+        atoms.calc.set(gpts=(g, g, g),
+                       occupations=FermiDirac(0.1),
+                       kpts=[1, 1, 1])
+        energies = []
+        forces = []
+        for i in range(25):
+            x = self.L / g * i / 48
+            atoms[0].x = x
+            e = atoms.calc.get_potential_energy(atoms,
+                                                force_consistent=True)
+            f = atoms.get_forces()[0, 0]
+            energies.append(e)
+            forces.append(f)
+
+        return {'energies': energies,
+                'forces': forces}
 
     def analyse(self):
         self.summary_header = [('name', '')] + [
             ('dE(h=%.2f)' % (self.L / g), 'meV') for g in self.gs]
 
         for name, data in self.data.items():
-            self.results[name] = [1000 * data[g][:, 0].ptp() for g in self.gs]
-            
-    def add_options(self, parser):
-        Task.add_options(self, parser)
-
-        egg = optparse.OptionGroup(parser, 'Eggbox')
-        egg.add_option('-g', '--grid-point-range', default='20,40',
-                       help='...')
-        parser.add_option_group(egg)
-
-    def parse(self, opts, args):
-        Task.parse(self, opts, args)
-
-        g1, g2 = (int(x) for x in opts.grid_point_range.split(','))
-        self.gs = range(g1, g2 + 1, 4)
+            symbol, g = name.split('-')
+            g = int(g)
+            de = data['energies'].ptp()
+            if symbol not in self.results:
+                self.results[symbol] = [None] * len(self.gs)
+            self.results[symbol][self.gs.index(g)] = de * 1000
 
 
 if __name__ == '__main__':
