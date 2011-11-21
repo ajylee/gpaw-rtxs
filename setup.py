93208db6ae8f242674a52e7c4dcea0c698166313
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2011-11-21 11:19:25 +0000

    Fail if customize file not fould.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8547 6419b465-d902-0410-997c-be2266baf723

--- setup.py
+++ setup.py
@@ -115,9 +115,8 @@ compiler = None
 
 scalapack = False
 hdf5 = False
-#User provided customizations
-if os.path.isfile(customize):
-    execfile(customize)
+
+execfile(customize)
 
 if platform_id != '':
     my_platform = distutils.util.get_platform() + '-' + platform_id
