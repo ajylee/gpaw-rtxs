4242eca3bec6935c93d337a356a8e9e560972408
juya <juya@6419b465-d902-0410-997c-be2266baf723>
2011-12-01 23:31:27 +0000

    make ecut smoother
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8602 6419b465-d902-0410-997c-be2266baf723

--- gpaw/response/cell.py
+++ gpaw/response/cell.py
@@ -24,7 +24,7 @@ def set_Gvectors(acell, bcell, nG, Ecut, q=[0., 0., 0.]):
     for i in range(3):
         a = acell[i]
         Gcut = sqrt(2*Ecut[i])
-        Gmax[i] = sqrt(a[0]**2 + a[1]**2 + a[2]**2) * Gcut/ (2*pi)
+        Gmax[i] = sqrt(a[0]**2 + a[1]**2 + a[2]**2) * Gcut/ (2*pi) + 1
      
     Nmax = 2 * Gmax + 1
     assert (nG - Nmax >=0).all() # to prevent too many planewaves
