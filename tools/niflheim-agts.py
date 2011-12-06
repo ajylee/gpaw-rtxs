03ac0b399e59feeb73baea55d0a0a30edcef4665
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2011-12-06 07:18:07 +0000

    Run AGTS tests with hdf.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8615 6419b465-d902-0410-997c-be2266baf723

--- tools/niflheim-agts.py
+++ tools/niflheim-agts.py
@@ -20,8 +20,10 @@ cd agts/gpaw&& \
 source /home/camp/modulefiles.sh&& \
 module load NUMPY&& \
 module load open64/4.2.3-0 && \
+module load openmpi/1.3.3-1.el5.fys.open64.4.2.3 && \
+module load hdf5/1.8.6-5.el5.fys.open64.4.2.3.openmpi.1.3.3 && \
 python setup.py --remove-default-flags --customize=\
-doc/install/Linux/Niflheim/el5-xeon-open64-goto2-1.13-acml-4.4.0.py \
+doc/install/Linux/Niflheim/el5-xeon-open64-goto2-1.13-acml-4.4.0-hdf.py \
 build_ext" | ssh thul bash""")
 
 cmd("""echo "\
