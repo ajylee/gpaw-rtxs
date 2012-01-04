57bb0d3c79d0249715c5e3f2c457229c275e77f2
jesswe <jesswe@6419b465-d902-0410-997c-be2266baf723>
2012-01-04 09:24:24 +0000

    optB88 exchange for optB88-vdW
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8682 6419b465-d902-0410-997c-be2266baf723

--- gpaw/xc/libxc_functionals.py
+++ gpaw/xc/libxc_functionals.py
@@ -41,6 +41,7 @@ libxc_functionals = {
     'GGA_X_MPBE': 122,
     'GGA_X_XPBE': 123,
     'GGA_X_OPTPBE': 124,
+    'GGA_X_OPTB88': 125,
     'GGA_C_PBE': 130,
     'GGA_C_LYP': 131,
     'GGA_C_P86': 132,
