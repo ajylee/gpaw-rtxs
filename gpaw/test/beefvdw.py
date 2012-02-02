c8c46f3eb2eca14e197270fc68447664eccb4959
jesswe <jesswe@6419b465-d902-0410-997c-be2266baf723>
2012-02-02 15:24:53 +0000

    fixed assertions
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8774 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/beefvdw.py
+++ gpaw/test/beefvdw.py
@@ -8,9 +8,9 @@ h2.center(vacuum=3.)
 calc = GPAW(xc=xc,txt=None)
 h2.set_calculator(calc)
 e = h2.get_potential_energy()
-assert e - -7.927315 < 1.e-6
+assert abs(e - -7.927315) < 1.e-6
 f = h2.get_forces()
 f0 = f[0].sum()
 f1 = f[1].sum()
 assert abs(f0 + f1) < 1.e-10
-assert f0 - 1.95088181246 < 1.e-10
+assert abs(f0 - 1.95088181246) < 1.e-10
