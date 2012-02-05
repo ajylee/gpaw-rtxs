c496a01c809ada4ec24cd0390a383b739db537b3
jesswe <jesswe@6419b465-d902-0410-997c-be2266baf723>
2012-02-05 13:25:14 +0000

    BEEF-vdW test expanded to include also a test of the BEEF-vdW ensemble error estimate
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8792 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/beefvdw.py
+++ gpaw/test/beefvdw.py
@@ -1,16 +1,42 @@
 from ase import *
 from gpaw import GPAW
+from gpaw.xc.bee import BEEF_Ensemble
+import numpy as np
 
 xc = 'BEEF-vdW'
-d = 0.8
-h2 = Atoms('H2', [[0.0,0.0,0.0],[0.0,0.0,d]], pbc=False)
+d = 0.75
+
+# H2 molecule
+h2 = Atoms('H2',[[0.,0.,0.],[0.,0.,d]])
 h2.center(vacuum=3.)
-calc = GPAW(xc=xc,txt=None)
+cell = h2.get_cell()
+calc = GPAW(xc=xc)
 h2.set_calculator(calc)
-e = h2.get_potential_energy()
-assert abs(e - -7.927315) < 1.e-6
+e_h2 = h2.get_potential_energy()
 f = h2.get_forces()
+ens = BEEF_Ensemble(calc)
+de_h2 = ens.get_ensemble_energies()
+del h2, calc, ens
+
+# H atom
+h = Atoms('H')
+h.set_cell(cell)
+h.center()
+calc = GPAW(xc=xc, spinpol=True)
+h.set_calculator(calc)
+e_h = h.get_potential_energy()
+ens = BEEF_Ensemble(calc)
+de_h = ens.get_ensemble_energies()
+
+# forces
 f0 = f[0].sum()
 f1 = f[1].sum()
 assert abs(f0 + f1) < 1.e-10
-assert abs(f0 - 1.95088181246) < 1.e-10
+assert abs(f0 - 0.534993531493) < 1.e-10
+
+# binding energy
+E_bind = 2*e_h - e_h2
+dE_bind = 2*de_h[:] - de_h2[:]
+dE_bind = np.std(dE_bind)
+assert abs(E_bind - 4.8738652912) < 1.e-10
+assert abs(dE_bind - 0.193965700193) < 1.e-10
