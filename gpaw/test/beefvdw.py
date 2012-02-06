5cefa1850fcc06cbff20f2bc39aaa5a7e2566208
jesswe <jesswe@6419b465-d902-0410-997c-be2266baf723>
2012-02-06 11:11:11 +0000

    fixed asertions
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8798 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/beefvdw.py
+++ gpaw/test/beefvdw.py
@@ -8,7 +8,7 @@ d = 0.75
 
 # H2 molecule
 h2 = Atoms('H2',[[0.,0.,0.],[0.,0.,d]])
-h2.center(vacuum=3.)
+h2.center(vacuum=2.)
 cell = h2.get_cell()
 calc = GPAW(xc=xc)
 h2.set_calculator(calc)
@@ -32,11 +32,11 @@ de_h = ens.get_ensemble_energies()
 f0 = f[0].sum()
 f1 = f[1].sum()
 assert abs(f0 + f1) < 1.e-10
-assert abs(f0 - 0.534993531493) < 1.e-10
+assert abs(f0 - 1.04407657919) < 1.e-6
 
 # binding energy
 E_bind = 2*e_h - e_h2
 dE_bind = 2*de_h[:] - de_h2[:]
 dE_bind = np.std(dE_bind)
-assert abs(E_bind - 4.8738652912) < 1.e-10
-assert abs(dE_bind - 0.193965700193) < 1.e-10
+assert abs(E_bind - 5.1263857048) < 1.e-6
+assert abs(dE_bind - 0.206959770215 ) < 1.e-3
