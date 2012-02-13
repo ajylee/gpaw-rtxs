65ccec72a2a50cf58daa8150e231ed62f5d095f3
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2012-02-13 12:38:08 +0000

    Fix inverse fft when using numpy's fft and odd sized grids.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@8851 6419b465-d902-0410-997c-be2266baf723

--- gpaw/fftw.py
+++ gpaw/fftw.py
@@ -73,10 +73,10 @@ class FFTPlanB:
         if self.in_R.dtype == float:
             self.out_R[:] = np.fft.rfftn(self.in_R)
         elif self.out_R.dtype == float:
-            self.out_R[:] = np.fft.irfftn(self.in_R)
+            self.out_R[:] = np.fft.irfftn(self.in_R, self.out_R.shape)
             self.out_R *= self.out_R.size
         elif self.sign == 1:
-            self.out_R[:] = np.fft.ifftn(self.in_R)
+            self.out_R[:] = np.fft.ifftn(self.in_R, self.out_R.shape)
             self.out_R *= self.out_R.size
         else:
             self.out_R[:] = np.fft.fftn(self.in_R)
