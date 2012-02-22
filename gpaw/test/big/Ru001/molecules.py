4bb1ee77926314b33215b12fbb557171697522df
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2012-02-22 11:58:17 +0000

    BFGSLineSearch takes too big step for NO molecule.  stencils=(3,3) is now the default.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/tags/0.9.0@8914 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/big/Ru001/molecules.py
+++ gpaw/test/big/Ru001/molecules.py
@@ -1,5 +1,5 @@
 from ase.structure import molecule
-from ase.optimize import QuasiNewton
+from ase.optimize import BFGS
 from gpaw import GPAW
 
 for name in ['H2', 'N2', 'O2', 'NO']:
@@ -9,10 +9,9 @@ for name in ['H2', 'N2', 'O2', 'NO']:
         mol.translate((0, 0.1, 0))
     calc = GPAW(xc='PBE',
                 h=0.2,
-                stencils=(3, 3),
                 txt=name + '.txt')
     mol.set_calculator(calc)
   
-    opt = QuasiNewton(mol, logfile=name + '.log', trajectory=name + '.traj')
+    opt = BFGS(mol, logfile=name + '.log', trajectory=name + '.traj')
     opt.run(fmax=0.05)
     calc.write(name)
