bc9767c9e20b9aae1bef9abe4a5bab4e4bf912ac
Andrew Lee <ajylee@gmail.com>
2015-01-27 19:34:53 -0800

    Move calc_setup.orthonormalize to :mod:`orthonormalize`

--- gpaw/rtxs/orthonormalize.py
+++ gpaw/rtxs/orthonormalize.py
@@ -29,3 +29,15 @@ def orthogonalize(wfs, kpt, a_xG):
                 orthogonalized[ii] += a_xG[ii]
 
     return orthogonalized
+
+
+def orthonormalize(calc, unocc_photoelectron_indices):
+
+    idx = unocc_photoelectron_indices
+
+    copy = [kpt.psit_nG.copy() for kpt in calc.wfs.kpt_u]
+
+    calc.wfs.orthonormalize()
+
+    for copy_psit_nG, calc_kpt in zip(copy, calc.wfs.kpt_u):
+        calc_kpt.psit_nG[idx] = copy_psit_nG[idx]
